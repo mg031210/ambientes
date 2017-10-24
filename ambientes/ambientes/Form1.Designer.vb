20a55faf447000d4c7f38a107f06f2d6331b749c
mg031210 <mg031210@gmail.com>
2017-10-23 20:26:05 -0500

    Ya tiene el menu fijo , da el visto bueno

--- ambientes/ambientes/Form1.Designer.vb
+++ ambientes/ambientes/Form1.Designer.vb
@@ -35,12 +35,27 @@ Partial Class Form1
         Me.Label5 = New System.Windows.Forms.Label()
         Me.TextBox5 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.UsuarioToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SocioToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ProveedorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InsertarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ActualizarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InsertarToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ActualizarToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InsertarToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ActualizarToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'nom
         '
         Me.nom.AccessibleName = "nom"
-        Me.nom.Location = New System.Drawing.Point(193, 120)
+        Me.nom.Location = New System.Drawing.Point(193, 118)
         Me.nom.Name = "nom"
         Me.nom.Size = New System.Drawing.Size(177, 20)
         Me.nom.TabIndex = 0
@@ -50,7 +65,7 @@ Partial Class Form1
         Me.Nombre.AutoSize = True
         Me.Nombre.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Nombre.ForeColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Nombre.Location = New System.Drawing.Point(90, 120)
+        Me.Nombre.Location = New System.Drawing.Point(90, 118)
         Me.Nombre.Name = "Nombre"
         Me.Nombre.Size = New System.Drawing.Size(71, 20)
         Me.Nombre.TabIndex = 1
@@ -80,7 +95,7 @@ Partial Class Form1
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.ForeColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Label2.Location = New System.Drawing.Point(17, 208)
+        Me.Label2.Location = New System.Drawing.Point(17, 206)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(144, 20)
         Me.Label2.TabIndex = 5
@@ -89,7 +104,7 @@ Partial Class Form1
         'TextBox2
         '
         Me.TextBox2.AccessibleName = "nom"
-        Me.TextBox2.Location = New System.Drawing.Point(193, 210)
+        Me.TextBox2.Location = New System.Drawing.Point(193, 208)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(177, 20)
         Me.TextBox2.TabIndex = 4
@@ -99,7 +114,7 @@ Partial Class Form1
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label3.ForeColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Label3.Location = New System.Drawing.Point(77, 256)
+        Me.Label3.Location = New System.Drawing.Point(77, 243)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(84, 20)
         Me.Label3.TabIndex = 7
@@ -108,7 +123,7 @@ Partial Class Form1
         'TextBox3
         '
         Me.TextBox3.AccessibleName = "nom"
-        Me.TextBox3.Location = New System.Drawing.Point(193, 256)
+        Me.TextBox3.Location = New System.Drawing.Point(193, 243)
         Me.TextBox3.Name = "TextBox3"
         Me.TextBox3.Size = New System.Drawing.Size(300, 20)
         Me.TextBox3.TabIndex = 6
@@ -118,7 +133,7 @@ Partial Class Form1
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label4.ForeColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Label4.Location = New System.Drawing.Point(82, 296)
+        Me.Label4.Location = New System.Drawing.Point(82, 284)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(79, 20)
         Me.Label4.TabIndex = 9
@@ -127,7 +142,7 @@ Partial Class Form1
         'TextBox4
         '
         Me.TextBox4.AccessibleName = "nom"
-        Me.TextBox4.Location = New System.Drawing.Point(193, 298)
+        Me.TextBox4.Location = New System.Drawing.Point(193, 286)
         Me.TextBox4.Name = "TextBox4"
         Me.TextBox4.Size = New System.Drawing.Size(127, 20)
         Me.TextBox4.TabIndex = 8
@@ -160,12 +175,120 @@ Partial Class Form1
         Me.Button1.Text = "Registrar"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.UsuarioToolStripMenuItem, Me.SocioToolStripMenuItem, Me.ProveedorToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(580, 24)
+        Me.MenuStrip1.TabIndex = 13
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'UsuarioToolStripMenuItem
+        '
+        Me.UsuarioToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.InsertarToolStripMenuItem2, Me.ActualizarToolStripMenuItem2})
+        Me.UsuarioToolStripMenuItem.Name = "UsuarioToolStripMenuItem"
+        Me.UsuarioToolStripMenuItem.Size = New System.Drawing.Size(59, 20)
+        Me.UsuarioToolStripMenuItem.Text = "Usuario"
+        '
+        'SocioToolStripMenuItem
+        '
+        Me.SocioToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.InsertarToolStripMenuItem, Me.ActualizarToolStripMenuItem})
+        Me.SocioToolStripMenuItem.Name = "SocioToolStripMenuItem"
+        Me.SocioToolStripMenuItem.Size = New System.Drawing.Size(48, 20)
+        Me.SocioToolStripMenuItem.Text = "Socio"
+        '
+        'ProveedorToolStripMenuItem
+        '
+        Me.ProveedorToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.InsertarToolStripMenuItem1, Me.ActualizarToolStripMenuItem1})
+        Me.ProveedorToolStripMenuItem.Name = "ProveedorToolStripMenuItem"
+        Me.ProveedorToolStripMenuItem.Size = New System.Drawing.Size(73, 20)
+        Me.ProveedorToolStripMenuItem.Text = "Proveedor"
+        '
+        'InsertarToolStripMenuItem
+        '
+        Me.InsertarToolStripMenuItem.Name = "InsertarToolStripMenuItem"
+        Me.InsertarToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.InsertarToolStripMenuItem.Text = "Insertar"
+        '
+        'ActualizarToolStripMenuItem
+        '
+        Me.ActualizarToolStripMenuItem.Name = "ActualizarToolStripMenuItem"
+        Me.ActualizarToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ActualizarToolStripMenuItem.Text = "Actualizar"
+        '
+        'InsertarToolStripMenuItem1
+        '
+        Me.InsertarToolStripMenuItem1.Name = "InsertarToolStripMenuItem1"
+        Me.InsertarToolStripMenuItem1.Size = New System.Drawing.Size(152, 22)
+        Me.InsertarToolStripMenuItem1.Text = "Insertar"
+        '
+        'ActualizarToolStripMenuItem1
+        '
+        Me.ActualizarToolStripMenuItem1.Name = "ActualizarToolStripMenuItem1"
+        Me.ActualizarToolStripMenuItem1.Size = New System.Drawing.Size(152, 22)
+        Me.ActualizarToolStripMenuItem1.Text = "Actualizar"
+        '
+        'InsertarToolStripMenuItem2
+        '
+        Me.InsertarToolStripMenuItem2.Name = "InsertarToolStripMenuItem2"
+        Me.InsertarToolStripMenuItem2.Size = New System.Drawing.Size(152, 22)
+        Me.InsertarToolStripMenuItem2.Text = "Insertar"
+        '
+        'ActualizarToolStripMenuItem2
+        '
+        Me.ActualizarToolStripMenuItem2.Name = "ActualizarToolStripMenuItem2"
+        Me.ActualizarToolStripMenuItem2.Size = New System.Drawing.Size(152, 22)
+        Me.ActualizarToolStripMenuItem2.Text = "Actualizar"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(267, 37)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(236, 20)
+        Me.TextBox6.TabIndex = 16
+        '
+        'Button2
+        '
+        Me.Button2.BackgroundImage = Global.ambientes.My.Resources.Resources.search_148820_960_720
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(509, 27)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(47, 38)
+        Me.Button2.TabIndex = 17
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label6.Location = New System.Drawing.Point(99, 325)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(62, 20)
+        Me.Label6.TabIndex = 18
+        Me.Label6.Text = "Status"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Activo", "Inactivo"})
+        Me.ComboBox1.Location = New System.Drawing.Point(193, 327)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 19
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.ClientSize = New System.Drawing.Size(580, 384)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox6)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.TextBox5)
@@ -179,8 +302,12 @@ Partial Class Form1
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Nombre)
         Me.Controls.Add(Me.nom)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Menu_registro"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -199,4 +326,18 @@ Partial Class Form1
     Friend WithEvents Label5 As Label
     Friend WithEvents TextBox5 As TextBox
     Friend WithEvents Button1 As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents UsuarioToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents InsertarToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents ActualizarToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents SocioToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents InsertarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ActualizarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ProveedorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents InsertarToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ActualizarToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents ComboBox1 As ComboBox
 End Class
--- ambientes/ambientes/Form1.vb
+++ ambientes/ambientes/Form1.vb
@@ -1,4 +1,5 @@
 ﻿Public Class Form1
+
     Private Sub Nombre_Click(sender As Object, e As EventArgs) Handles Nombre.Click
 
     End Sub
@@ -20,6 +21,26 @@
     End Sub
 
     Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged_1(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub SocioToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SocioToolStripMenuItem.Click
 
     End Sub
 End Class
