8a3a2a76c2a4c13c6272e6fffb2e6beda8e15cf7
mg031210 <mg031210@gmail.com>
2017-11-17 14:11:40 -0600

    .

--- Ambientes2/Ambientes2/Globales/Conexion.vb
+++ Ambientes2/Ambientes2/Globales/Conexion.vb
@@ -2,7 +2,7 @@
 
 Public Class Conexion
     Public Sub New()
-        cnx.ConnectionString = "server=localhost; user=root; password= ; database=mydb"
+        cnx.ConnectionString = "server=localhost; user=root; password=obsession; database=mybd"
         If cnx.State <> ConnectionState.Open Then
             Try
                 cnx.Open()
